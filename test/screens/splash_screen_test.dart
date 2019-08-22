bedbf3840e777e7cc9f0928d86c20a9a9d45979c
Billy Stetson <billy@foriatickets.com>
2019-08-21 22:27:16 -0500

    FM-41: Splash Screen Test
    Test that the splash screen brings you to the right screens

---
+++ test/screens/splash_screen_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:foria/main.dart';
+import 'package:foria/screens/login.dart';
+import 'package:foria/screens/splash_screen.dart';
+import 'package:foria/utils/auth_utils.dart';
+import 'package:mockito/mockito.dart';
+
+void main() {
+
+  setUp(() {
+//    when(isUserLoggedIn(true)).thenAnswer((_) => Future.value(false)); ///TBU when auth class is made
+  });
+
+
+
+  testWidgets('navigation to login screen', (WidgetTester tester) async {
+
+
+    await tester.pumpWidget(MaterialApp(
+        home: SplashScreen(),
+      navigatorKey: navigatorKey,
+      routes: {
+        Login.routeName: (context) => Login(),
+      },
+    ));
+
+    await tester.pump(Duration(seconds: 15)
+    );
+
+    await tester.pumpAndSettle();
+
+    expect(find.byType(Login), findsOneWidget);
+
+  });
+}
